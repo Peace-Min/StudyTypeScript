4574c0de2e28a5fef0734274f6c7d1f1a166887f
Peace-Min <20181740@edu.hanbat.ac.kr>
2025-02-18 22:47:46 +0900

    ts는 js와 달리 function에서 자료형을 정의형 할 수 있어서 컴파일러가 추론이 가능하다. -> 에러 사전 방지가능 -> 자료형을 추론이 가능해서 해당 자료형에 대한 API를 사용 가능하다.
    js에서 ts처럼 함수의 파라미터의 제너릭을 설정하기 위해 jsDoc을 위해 파라미터의 제너릭을 설정할 수 잇다.
    
    // @ts-check -> js에서도 ts의 기능을 일부 사용가능.
    
    .ts 확장자에서 바로 실행하면 브라우저는 .ts 확장자를 인식하지못함
    그렇기 때문에 .ts 확장자를 .js로 변환하는 작업이 필요
    -> 컴파일(compile)이라 지칭.
    
    npm = node package manager
    -> node 기반으로 자바스크립트 라이브러리 설치 가능
    -> tsc : 타입스크립트 컴파일
    
    TODO : Ts 기본 자료형

---
+++ class-note/1_type_basic.ts
@@ -0,0 +1,10 @@
+// var str = "hello world";
+
+// const str: string = 'hello';
+
+let str : string = 'hello';
+
+let num:number = 10;
+let arr : Array<number> = [1, 2, 3];
+let heros : Array<string> = ['Capt',10];
+var items: number[] = [1, 2, 3];
