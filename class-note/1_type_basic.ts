1558f01f019582025d3965693370eb622cfc3624
Peace-Min <20181740@edu.hanbat.ac.kr>
2025-02-19 23:25:34 +0900

    // 특정 위치에 타입을 지정 가능한 타입 -> 튜플
    ts에는 옵셔널 파라미터가 존재하는데
    c#에서 매개변수로 int a=null과 동일한 개념이다 a?: number

--- class-note/1_type_basic.ts
+++ class-note/1_type_basic.ts
@@ -4,7 +4,27 @@
 
 let str : string = 'hello';
 
+// Ts 숫자
 let num:number = 10;
+
+// TS 배열
 let arr : Array<number> = [1, 2, 3];
 let heros : Array<string> = ['Capt',10];
 var items: number[] = [1, 2, 3];
+
+// TS 튜플
+// 특정 위치에 타입을 지정 가능한 타입 -> 튜플
+let address : [string,number] = ['강남',0];
+
+// TS 객체
+let obj: object = {};
+// let person: object = {
+//     name : 'capt',
+//     age:100,
+// }
+let person : {name : string, age:number}={
+    name:'thor',
+    age:100,
+}
+
+let show = true;
